ba348ebae039d221677e64ebe4f5c230c9f1c53a
drTopy <itutoru@outlook.com>
2016-03-16 22:02:10 -0400

    v1 done, need to work a few bugs out

---
+++ PokedexT/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokedexT
+//
+//  Created by Thiago Costa on 3/15/16.
+//  Copyright © 2016 Thiago Costa. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- PokedexT/PokemonDetailVC.swift
+++ PokedexT/PokemonDetailVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
     //IBOutlets
     @IBOutlet var mainImg: UIImageView!
     @IBOutlet var pokemonNamelbl: UILabel!
@@ -25,26 +25,52 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet var evoLbl: UILabel!
     
     @IBAction func backBtnPressed(sender: AnyObject) {
-    dismissViewControllerAnimated(true, completion: nil)
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
     var pokemon: Pokemon!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         pokemonNamelbl.text = pokemon.name
+        let img =  UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
+        
+        pokemon.downloadPokemonDetails { () -> () in
+            //this will be called after download is done
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        descriptionLbl.text = pokemon.desc
+        typeLbl.text = pokemon.type
+        defenseLbl.text = pokemon.defense   
+        heightLbl.text = pokemon.height
+        pokedexIdLbl.text = "\(pokemon.pokedexId)"
+        weightLbl.text = pokemon.weight
+        baseAttackLbl.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        } else {
+            
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+            
+            if pokemon.nextEvolutionLvl != "" {
+                str += " - LVL \(pokemon.nextEvolutionLvl)"
+            }else {
+                
+            }
+        }
         
     }
     
-
+    
 }
